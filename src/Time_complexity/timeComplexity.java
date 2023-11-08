90d83b06f797641744dbabbf098e158d300eb0e2
Joo-Veloper <psjoo3515@gmail.com>
2023-11-08 20:19:39 +0900

    TimeComplexity

--- src/Time_complexity/timeComplexity.java
+++ src/Time_complexity/timeComplexity.java
@@ -1,4 +1,13 @@
 package Time_complexity;
-
+// 시간복잡도 O(n)
 public class timeComplexity {
+    public static void main(String[] args) {
+        int findNumber = (int)(Math.random() * 100);
+        for (int i = 0; i < 100; i++) {
+            if(i == findNumber) {
+                System.out.println(i);
+                break;
+            }
+        }
+    }
 }
