8c0b06328e12b4aedd68e1db13fcd24d5be24c9f
Joo-Veloper <psjoo3515@gmail.com>
2023-11-09 09:22:15 +0900

    average

---
+++ src/DataStructure/Average.java
@@ -0,0 +1,22 @@
+package DataStructure;
+
+import java.util.Scanner;
+
+public class Average {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int[] A = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            A[i] = scanner.nextInt();
+        }
+        int sum = 0;
+        int max = 0;
+        for (int i = 0; i < N; i++) {
+            if (A[i] > max) max = A[i];
+            sum = sum + A[i];
+        }
+        System.out.println(sum * 100.0/ max /N);
+    }
+}
