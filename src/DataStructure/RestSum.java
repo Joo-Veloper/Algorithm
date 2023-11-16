87c4274e35a6d73ebdade56d029992936f4c6a36
Joo-Veloper <psjoo3515@gmail.com>
2023-11-16 09:57:08 +0900

    RestSum

--- src/DataStructure/RestSum.java
+++ src/DataStructure/RestSum.java
@@ -1,7 +1,38 @@
 package DataStructure;
 
+import java.util.Scanner;
+
 public class RestSum {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        long[] S = new long[N];
+        long[] C = new long[M];
+        long answer = 0;
 
+        // Initialize the first element of S separately
+        S[0] = sc.nextInt();
+        for (int i = 1; i < N; i++) {
+            S[i] = S[i - 1] + sc.nextInt();
+        }
+
+        for (int i = 0; i < N; i++) {
+            int re = (int) (S[i] % M);
+
+            if (re == 0) {
+                answer++;
+            }
+
+            C[re]++;
+        }
+
+        for (int i = 0; i < M; i++) {
+            if (C[i] > 1) {
+                answer = answer + (C[i] * (C[i] - 1) / 2);
+            }
+        }
+
+        System.out.println(answer);
     }
 }
