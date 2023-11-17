9217357ec4a31744be23eb4c8bb8077b25dfc7a2
Joo-Veloper <psjoo3515@gmail.com>
2023-11-17 09:40:54 +0900

    mid

---
+++ src/Basic/BigSmall.java
@@ -0,0 +1,37 @@
+package Basic;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
+public class BigSmall {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("세 수의 중앙값");
+        System.out.println("a의 값");
+        int a = sc.nextInt();
+        System.out.println("b의 값");
+        int b = sc.nextInt();
+        System.out.println("c의값");
+        int c = sc.nextInt();
+        System.out.println("중앙값" + mid(a, b, c) + "입니다.");
+    }
+    static int mid(int a, int b, int c){
+        if(a>=b) {
+            if(b>=c) {
+                return b;
+            } else if (a <=c) {
+                return a;
+            }else {
+                return c;
+            }
+        }else if (a>c){
+            return a;
+        } else if (b>c) {
+            return c;
+        }else {
+            return b;
+        }
+    }
+    
+}
