aa9ba5ccc7f61f89aceecb493d2903480d530b64
Joo-Veloper <psjoo3515@gmail.com>
2023-11-19 14:25:04 +0900

    Alternative

---
+++ src/Basic/Alternative1.java
@@ -0,0 +1,23 @@
+package Basic;
+
+import java.util.Scanner;
+
+public class Alternative1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        System.out.println("+와 -를 번갈아가며 n개 출력합니다.");
+        do {
+            System.out.print("n 값 : ");
+            n = sc.nextInt();
+        } while (n <= 0);
+
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                System.out.print("+");
+            } else {
+                System.out.print("-");
+            }
+        }
+    }
+}
---
+++ src/Basic/Alternative2.java
@@ -0,0 +1,22 @@
+package Basic;
+
+import java.util.Scanner;
+
+public class Alternative2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        System.out.println("+와 -를 번갈아가며 n개 출력합니다.");
+        do {
+            System.out.print("n 값 : ");
+            n = sc.nextInt();
+        } while (n <= 0);
+
+        for (int i = 0; i < n/2; i++) { // for 문은 +-를 2회 출력 n이 12 면 6번 15이면 7번 출력
+            System.out.print("+-");
+        }
+        if (n%2!=0){
+            System.out.print("+");
+        }
+    }
+}
