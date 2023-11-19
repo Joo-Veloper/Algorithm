8c6b2969710a717e8e2b288db7f7a68cf1c167c5
Joo-Veloper <psjoo3515@gmail.com>
2023-11-19 15:31:23 +0900

    9x9

---
+++ src/Basic/chap01/Multi99.java
@@ -0,0 +1,14 @@
+package Basic.chap01;
+
+public class Multi99 {
+    public static void main(String[] args) {
+        System.out.println("9 X 9");
+
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= 9 ; j++) {
+                System.out.printf("%3d", i * j);
+            }
+            System.out.println();
+        }
+    }
+}
