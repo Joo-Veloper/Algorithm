dcdebe50ea8de1eb689676f39acebd4875df5315
Joo-Veloper <psjoo3515@gmail.com>
2023-11-25 22:41:33 +0900

    Prime

---
+++ src/Basic/Chap02/PrimeNumber1.java
@@ -0,0 +1,14 @@
+package Basic.Chap02;
+
+public class PrimeNumber1 {
+    public static void main(String[] args) {
+        int counter = 0;
+
+        for (int n = 2; n <= 1000 ; n++) {
+            int i;
+            for (i = 2; i < n; i++) {
+
+            }
+        }
+    }
+}
