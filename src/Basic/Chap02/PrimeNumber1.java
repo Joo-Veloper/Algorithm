f060c945778ae0af5aad33d1aa72e25a05e0a181
Joo-Veloper <psjoo3515@gmail.com>
2023-11-25 22:44:04 +0900

    Prime

--- src/Basic/Chap02/PrimeNumber1.java
+++ src/Basic/Chap02/PrimeNumber1.java
@@ -7,8 +7,14 @@ public class PrimeNumber1 {
         for (int n = 2; n <= 1000 ; n++) {
             int i;
             for (i = 2; i < n; i++) {
-
+                counter++;
+                if(n%i == 0)
+                    break;
+            }
+            if(n==i){
+                System.out.println(n);
             }
         }
+        System.out.println("나눗셈을 수행한 횟수 : " + counter);
     }
 }
