d7959e1d67c5fee839be2ea3de0c74ddfce9655b
Joo-Veloper <psjoo3515@gmail.com>
2023-12-10 19:37:03 +0900

    queue

---
+++ src/Basic/Chap04/IntQueue.java
@@ -0,0 +1,26 @@
+package Basic.Chap04;
+
+public class IntQueue {
+    private int[] que;
+    private int capacity;
+    private int front;
+    private int rear;
+    private int num;
+
+    public class EmptyIntQueueException extends RuntimeException{
+        public EmptyIntQueueException(){ }
+    }
+    public class OverflowIntQueueException extends RuntimeException {
+        public OverflowIntQueueException() {}
+    }
+
+    public IntQueue(int maxlen) {
+        num = front = rear = 0;
+        capacity = maxlen;
+        try {
+            que = new int[capacity];
+        }catch (OutOfMemoryError e) {
+            capacity = 0;
+        }
+    }
+}
