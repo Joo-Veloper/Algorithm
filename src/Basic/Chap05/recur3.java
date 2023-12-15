457b6d980d1f51762db0ce198fd1e9423d09e713
Joo-Veloper <psjoo3515@gmail.com>
2023-12-15 09:36:44 +0900

    recure refactoring

--- src/Basic/Chap05/recur3.java
+++ src/Basic/Chap05/recur3.java
@@ -4,10 +4,36 @@ import java.util.Scanner;
 
 public class recur3 {
     static void recur3(int n) {
-        if(n>0) {
-            recur3(n - 1);
-            recur3(n - 2);
-            System.out.println(n);
+        int[] ns = new int[100];
+        int[] st = new int[100];
+        int ptr = -1;
+        int sw = 0;
+
+        while (true) {
+            if (n > 0) {
+                ptr++;
+                ns[ptr] = n;
+                st[ptr] = sw;
+                if(sw == 0)
+                    n = n-1;
+                else if (sw == 1) {
+                    n = n - 2;
+                    sw = 0;
+                }
+                continue;
+
+            }
+            do{
+                n = ns[ptr];
+                sw = st[ptr--] + 1;
+                if(sw == 2) {
+                    System.out.println(n);
+                    if(ptr<0){
+                        return;
+                    }
+                }
+            }
+            while (sw == 2);
         }
     }
 
