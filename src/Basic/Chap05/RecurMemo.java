b1c600019b52bd4657f614189af11acad937921f
Joo-Veloper <psjoo3515@gmail.com>
2023-12-15 09:13:18 +0900

    hanoi

--- src/Basic/Chap05/RecurMemo.java
+++ src/Basic/Chap05/RecurMemo.java
@@ -14,7 +14,7 @@ public class RecurMemo {
                 recur(n-1);
                 System.out.println(n);
                 recur(n-2);
-                memo[n+1] = memo[n] + n + "\n" + memo[n-1];
+                memo[n+1] = memo[n] + n + "\n" + memo[n-1]; // momo 배열의 n+1 인덱스에 새로운 값을 저장합니다. 여기서 memo[n] + n + "\n" + memo[n-1]은 이전값 memo[n]에 n과 개행문자 ("\n")그리고 momo[n-1]을 합친 문자열을 의미합니다.
             }
             else {
                 memo[n+1] = "";
