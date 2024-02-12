5599f5b6d1c7d031695a9435b3a8dec9a4c04698
Seung Joo <psjoo3515@gmail.com>
2024-02-12 20:10:13 +0900

    NODE

---
+++ src/Basic/Chap08/DoubleLinkedList.java
@@ -0,0 +1,30 @@
+package Basic.Chap08;
+
+public class DoubleLinkedList<E> {
+    class Node<E>{
+        private E data;
+        private Node<E> prev;
+        private Node<E> next;
+
+        Node(){
+            data = null;
+            prev = next = this;
+        }
+
+        Node(E obj, Node<E> prev, Node<E> next) {
+            data = obj;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
+    private Node<E> head;
+    private Node<E> crnt;
+
+    public DoubleLinkedList() {
+        head = crnt = new Node<E>();
+    }
+
+    public boolean isEmpty() {
+        return head.next == head;
+    }
+}
