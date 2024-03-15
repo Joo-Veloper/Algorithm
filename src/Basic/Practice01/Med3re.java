878134aa4ac21d060c956bbefad15943d5dee760
Seung Joo <psjoo3515@gmail.com>
2024-03-15 18:22:41 +0900

    if 문의 판단

--- src/Basic/Practice01/Med3re.java
+++ src/Basic/Practice01/Med3re.java
@@ -1,28 +1,18 @@
 package Basic.Practice01;
-
-import java.util.HashMap;
-import java.util.Map;
-
-public class Med3 {
-    static int med3(int a, int b, int c) {
-        if (a >= b) {
-            if (b >= c) {
-                return b;
-            } else if (a <= c) {
-                return a;
-            }
-            else
-                return c;
-        }
-        else if(a > c) {
+/** if 문의 판단.
+ * if((b>=a&& c<= a) || (b<= a&& c>=a)에 주목합니다.
+ * 여기서 b>=a 및 v<=a 의 판단을 뒤집은 판단 (실질적으로 동일한 판단)이 계속하여, else 이후에서
+ * else if ((a >b, && c < b) || (b<=a&& c>b)로 수행됩니다. 즉, 처음 if가 성립하지 않는경우,
+ * 2번째 if에서도 (실질적으로) 같은 판단을 수행하므로, 효율이 나빠집니다.
+ **/
+public class Med3re {
+    static int med3(int a, int b, int c){
+        if ((b >= a && c <= a) || (b <= a && c >= a)) {
             return a;
-        } else if (b > c) {
-            return c;
-        } else {
+        } else if ((a > b && c < b) || (a < b && c > b)) {
             return b;
-        }
+        }return c;
     }
-
     public static void main(String[] args) {
         System.out.println("Med3(3,2,1) = " + med3(3,2,1));
         System.out.println("Med3(3,2,2) = " + med3(3,2,2));
