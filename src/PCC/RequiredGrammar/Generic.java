d49b8a94f89b86eb71b1a57e325c5f14e63ef342
Seung Joo <psjoo3515@gmail.com>
2024-03-22 22:53:00 +0900

    Generic Update

--- src/PCC/RequiredGrammar/Generic.java
+++ src/PCC/RequiredGrammar/Generic.java
@@ -3,20 +3,30 @@ package PCC.RequiredGrammar;
 import java.util.ArrayList;
 import java.util.List;
 
-/** 제네릭은 빌드 레벨ㄹ에서 타입을 체크ㅏ여 타입 안정성을 제공하고, 타입 체크와 형변환을 생략할 수 있게 해주어 코드 간결
+/** 제네릭은 빌드 레벨에서 타입을 체크ㅏ여 타입 안정성을 제공하고, 타입 체크와 형변환을 생략할 수 있게 해주어 코드 간결
  * List를 정의할때 <Integer>와 같이 타입을 강제 하는것 = 제네릭
  */
 
 public class Generic {
-    List list = new ArrayList();
-    list.add(10);
-    list.add("abc");
+    public static void main(String[] args) {
+        // Create a raw list
+        List list = new ArrayList();
+        list.add(10);
+        list.add("abc");
 
-    int sum1 = (int)list.get(0) + (int)list.get(1);
+        // Incorrect: You cannot add a string to an int directly
+        // int sum1 = (int) list.get(0) + (int) list.get(1);
 
-    List<Integer> genericList = new ArrayList<>();
-    genericList.add(10);
-    genericList.add("abc");
+        // Create a generic list of integers
+        List<Integer> genericList = new ArrayList<>();
+        genericList.add(10);
 
-    int sum2 = genericList.get(0) + genericList.get(1);
+        // Incorrect: You cannot add a string to an integer list
+        // genericList.add("abc");
+
+        // Correct: You can directly get integers from the list and perform operations
+        int sum2 = genericList.get(0) + Integer.parseInt(list.get(0).toString());
+
+        System.out.println("Sum: " + sum2);
+    }
 }
