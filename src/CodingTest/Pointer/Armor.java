e3ec40ccae53a7193d0832017cb593503cfb71c9
Seung Joo <psjoo3515@gmail.com>
2024-03-25 16:13:33 +0900

    Armor maker

---
+++ src/CodingTest/Pointer/Armor.java
@@ -0,0 +1,35 @@
+package CodingTest.Pointer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+public class Armor {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+        int M = Integer.parseInt(bf.readLine());
+        int[] A = new int[N];
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(A);
+        int count = 0;
+        int i = 0;
+        int j = N - 1;
+
+        while (i < j) {
+            if (A[i] + A[j] < M) {
+                i++;
+            } else if (A[i] + A[j] > M) {
+                j--;
+            }else{
+                count++;
+                j++;
+                j--;
+            }
+        }
+        System.out.println(count);
+        bf.close();
+    }
+}
