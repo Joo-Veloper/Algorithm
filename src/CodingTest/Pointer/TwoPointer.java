001b996c56afd13043afc12a73a8d3bd4f3eeadb
Seung Joo <psjoo3515@gmail.com>
2024-03-25 11:56:55 +0900

    Two Pointer Sudo

---
+++ src/CodingTest/Pointer/TwoPointer.java
@@ -0,0 +1,41 @@
+package CodingTest.Pointer;
+
+import java.util.*;
+
+public class TwoPointer {
+    /**
+     * sudo-Code
+     * Scanner 입력
+     * N 변수 저장
+     * ( count, start_index, end_indx, sum) 초기화
+     * while(end_ind != N) {
+     *     if(sum == N){}
+     *     else if(sum < N)
+     *     else
+     * }
+     * count
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int count = 1;
+        int start_index = 1;
+        int end_index = 1;
+        int sum = 1;
+        while (end_index != N) {
+            if (sum == N) {
+                count++;
+                end_index++;
+                sum = sum + end_index;
+            } else if (sum > N) {
+                sum = sum - start_index;
+                start_index++;
+            } else {
+                end_index++;
+                sum = sum + end_index;
+            }
+        }
+        System.out.println(count);
+    }
+}
