d5561129e4e9d134b2c7d74b43927783470a7375
Seung Joo <psjoo3515@gmail.com>
2024-04-07 16:19:05 +0900

    Stack Remove

--- src/PCC/Stack/RemoveSet.java
+++ src/PCC/Stack/RemoveSet.java
@@ -1,22 +1,22 @@
 package PCC.Stack;
 
 
+/* 알파벳 소문자로 구성된 문자열에서 같은 알파벳이 2개 붙어 있는 짝
+ * 짝을 찾은 다음에는 그 둘을 제거한 뒤 앞뒤로 문자열을 이어 붙입니다. 이과정을 반복해서 문자열을 모두 제거한다면
+ * 짝찌어 제거하기가 종료됩니다. 문자열 S가 주어졌을 때 짝찌어 제거하기를 성공적으로 수행할 수 있는지
+ * 반환하는 함수를 완성*/
 import java.util.Scanner;
 import java.util.Stack;
 
 public class RemoveSet {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
-        String result = String.valueOf(solution3(s));
+        Scanner scanner = new Scanner(System.in);
+        String inputString = scanner.nextLine();
+        int result = removeAdjacentPairs(inputString);
         System.out.println(result);
     }
 
-    /* 알파벳 소문자로 구성된 문자열에서 같은 알파벳이 2개 붙어 있는 짝
-    * 짝을 찾은 다음에는 그 둘을 제거한 뒤 앞뒤로 문자열을 이어 붙입니다. 이과정을 반복해서 문자열을 모두 제거한다면
-    * 짝찌어 제거하기가 종료됩니다. 문자열 S가 주어졌을 때 짝찌어 제거하기를 성공적으로 수행할 수 있는지
-    * 반환하는 함수를 완성*/
-    public static int solution3(String s) {
+    public static int removeAdjacentPairs(String s) {
         // 문자를 저장할 스택을 생성합니다.
         Stack<Character> stack = new Stack<>();
         // 입력받은 문자열 s의 각 문자를 순회합니다
