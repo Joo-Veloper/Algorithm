23f1758cf47a0e490628ddfcb6e5abfe2259e134
Seung Joo <psjoo3515@gmail.com>
2024-06-09 23:10:39 +0900

    Algorithm Test

---
+++ src/BaekJ/deep/weed.java
@@ -0,0 +1,15 @@
+package BaekJ.deep;
+
+
+public class weed {
+    public static void main(String[] args) {
+        String s = "         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |";
+
+        System.out.print(s);
+    }
+}
