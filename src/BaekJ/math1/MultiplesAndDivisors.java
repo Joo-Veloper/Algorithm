fe7e35edf118ce3b030424338c26bdb1addce589
Seung Joo <psjoo3515@gmail.com>
2024-07-06 20:14:03 +0900

    MultiplesAndDivisors

---
+++ src/BaekJ/math1/MultiplesAndDivisors.java
@@ -0,0 +1,32 @@
+package BaekJ.math1;
+
+import java.util.Scanner;
+
+public class MultiplesAndDivisors {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int first;
+        int second;
+
+
+        while (true) {
+
+            first = sc.nextInt();
+            second = sc.nextInt();
+
+            if(first == 0 && second == 0){
+                break;
+            }
+
+            if(second % first == 0) {
+                System.out.println("factor");
+            }
+            else if(first % second == 0) {
+                System.out.println("multiple");
+            }
+            else {
+                System.out.println("neither");
+            }
+        }
+    }
+}
