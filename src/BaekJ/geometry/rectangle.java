b41c23b6a73c401e17fcea5f877442e343fc3589
Seung Joo <psjoo3515@gmail.com>
2024-07-13 16:38:15 +0900

    Retangle

---
+++ src/BaekJ/geometry/rectangle.java
@@ -0,0 +1,19 @@
+package BaekJ.geometry;
+
+import java.util.Scanner;
+
+public class rectangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a;
+        int b;
+        int result;
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        result = a * b;
+
+        System.out.println(result);
+    }
+}
