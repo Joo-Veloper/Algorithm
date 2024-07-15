a6df7ab509f24d3a96cdcb92a12e99fad8e1f4c6
Seung Joo <psjoo3515@gmail.com>
2024-07-15 21:15:44 +0900

    수학은 체육

---
+++ src/BaekJ/geometry/MathGym.java
@@ -0,0 +1,13 @@
+package BaekJ.geometry;
+
+import java.util.Scanner;
+
+public class MathGym {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.println(4+(4*(n-1)));
+    }
+}
