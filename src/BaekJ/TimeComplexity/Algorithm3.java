9415d04442201259f80f683199789e11b71994ca
Seung Joo <psjoo3515@gmail.com>
2024-07-19 22:00:14 +0900

    알고리즘 수업 - 알고리즘의 수행 시간 3

---
+++ src/BaekJ/TimeComplexity/Algorithm3.java
@@ -0,0 +1,13 @@
+package BaekJ.TimeComplexity;
+
+import java.util.Scanner;
+
+public class Algorithm3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextInt();
+
+        System.out.println(n * n);
+        System.out.println(2);
+    }
+}
