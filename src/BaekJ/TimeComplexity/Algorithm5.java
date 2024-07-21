2881b20ca59a11fe3a153d52f6b056fef03fa0b6
Seung Joo <psjoo3515@gmail.com>
2024-07-21 15:05:56 +0900

    알고리즘 수업 - 알고리즘의 수행 시간5

---
+++ src/BaekJ/TimeComplexity/Algorithm5.java
@@ -0,0 +1,33 @@
+package BaekJ.TimeComplexity;
+
+import java.util.Scanner;
+
+public class Algorithm5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int sum;
+        int count;
+
+        sum = n * n * n;
+        count = 3;
+        System.out.println(sum);
+        System.out.println(count);
+    }
+}
+/*오늘도 서준이는 알고리즘의 수행시간 수업 조교를 하고 있다. 아빠가 수업한 내용을 학생들이 잘 이해했는지 문제를 통해서 확인해보자.
+
+입력의 크기 n이 주어지면 MenOfPassion 알고리즘 수행 시간을 예제 출력과 같은 방식으로 출력해보자.
+
+MenOfPassion 알고리즘은 다음과 같다.
+
+MenOfPassion(A[], n) {
+    sum <- 0;
+    for i <- 1 to n
+        for j <- 1 to n
+            for k <- 1 to n
+                sum <- sum + A[i] × A[j] × A[k]; # 코드1
+    return sum;
+}
+*/
