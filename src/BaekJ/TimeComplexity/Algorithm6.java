7a9fb11d8821101ed8daa7019483ee4505ce0719
Seung Joo <psjoo3515@gmail.com>
2024-07-22 20:59:22 +0900

    알고리즘 수업 - 알고리즘의 수행 시간6

---
+++ src/BaekJ/TimeComplexity/Algorithm6.java
@@ -0,0 +1,12 @@
+package BaekJ.TimeComplexity;
+
+import java.util.Scanner;
+
+public class Algorithm6 {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            long a = sc.nextInt();
+            System.out.println((a*(a-1)*(a-2))/6);
+            System.out.println(3);
+        }
+}
