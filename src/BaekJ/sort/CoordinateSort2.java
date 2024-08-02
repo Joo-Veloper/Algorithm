a070bcc86f78c1e4dcdd91eeb935706fe1fbe2f4
Seung Joo <psjoo3515@gmail.com>
2024-08-02 18:44:52 +0900

    좌표 정렬2

---
+++ src/BaekJ/sort/CoordinateSort2.java
@@ -0,0 +1,33 @@
+package BaekJ.sort;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class CoordinateSort2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] arr = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            arr[i][0] = sc.nextInt();
+            arr[i][1] = sc.nextInt();
+        }
+
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] a, int[] b) {
+                if (a[1] != b[1]) {
+                    return Integer.compare(a[1], b[1]);
+                }
+                return Integer.compare(a[0], b[0]);
+            }
+        });
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i][0] + " " + arr[i][1]);
+        }
+        sc.close();
+    }
+}
