d8830ceabba114125f5957e4c5e714fddfd2817f
Seung Joo <psjoo3515@gmail.com>
2024-08-31 23:36:47 +0900

    소수 구하기

---
+++ src/BaekJ/FactorsAndMultiplesAndDecimals/Prime.java
@@ -0,0 +1,34 @@
+package BaekJ.FactorsAndMultiplesAndDecimals;
+import java.util.Scanner;
+
+public class Prime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        sc.close();
+
+        pr(m, n);
+    }
+
+    public static void pr(int m, int n) {
+        boolean[] isPrime = new boolean[n + 1];
+        for (int i = 2; i <= n; i++) {
+            isPrime[i] = true;
+        }
+
+        for (int i = 2; i * i <= n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        for (int i = m; i <= n; i++) {
+            if (isPrime[i]) {
+                System.out.println(i);
+            }
+        }
+    }
+}
