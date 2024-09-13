b97e7a1f1de9f288c7ab9d5398d20c59e6e2ce33
Seung Joo <psjoo3515@gmail.com>
2024-09-13 18:39:57 +0900

    Queue 2

---
+++ src/BaekJ/stack/Queue2.java
@@ -0,0 +1,57 @@
+package BaekJ.stack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Queue2 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        Queue<Integer> queue = new LinkedList<>();
+        int n = Integer.parseInt(br.readLine());
+        int back = -1;
+
+        for (int i = 0; i < n; i++) {
+            String command = br.readLine();
+
+            if (command.startsWith("push")) {
+                int x = Integer.parseInt(command.split(" ")[1]);
+                queue.offer(x);
+                back = x;
+            } else if (command.equals("pop")) {
+                if (queue.isEmpty()) {
+                    sb.append(-1).append("\n");
+                } else {
+                    sb.append(queue.poll()).append("\n");
+                }
+            } else if (command.equals("size")) {
+                sb.append(queue.size()).append("\n");
+            } else if (command.equals("empty")) {
+                if (queue.isEmpty()) {
+                    sb.append(1).append("\n");
+                } else {
+                    sb.append(0).append("\n");
+                }
+            } else if (command.equals("front")) {
+                if (queue.isEmpty()) {
+                    sb.append(-1).append("\n");
+                } else {
+                    sb.append(queue.peek()).append("\n");
+                }
+            } else if (command.equals("back")) {
+                if (queue.isEmpty()) {
+                    sb.append(-1).append("\n");
+                } else {
+                    sb.append(back).append("\n");
+                }
+            }
+        }
+
+        System.out.println(sb.toString());
+    }
+}
