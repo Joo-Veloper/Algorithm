1af4b971d585307a7453e03f4eb9aa04d7f33b6a
Seung Joo <psjoo3515@gmail.com>
2024-09-21 22:28:35 +0900

    The Fashion Of Bera

---
+++ src/BaekJ/combination/FashionOfBara.java
@@ -0,0 +1,13 @@
+package BaekJ.combination;
+
+import java.util.Scanner;
+
+public class FashionOfBara {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.println(n * (n - 1));
+    }
+}
