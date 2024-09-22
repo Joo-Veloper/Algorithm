fcf43be979ef1bc4f3dd39079175fcc9277ad4a9
Seung Joo <psjoo3515@gmail.com>
2024-09-22 20:43:44 +0900

    Factorial

---
+++ src/BaekJ/combination/Factorial.java
@@ -0,0 +1,24 @@
+package BaekJ.combination;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println(factorial(n));
+    }
+
+    static int factorial(int n) {
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
+/*0보다 크거나 같은 정수 N이 주어진다. 이때, N!을 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 정수 N(0 ≤ N ≤ 12)이 주어진다.*/
