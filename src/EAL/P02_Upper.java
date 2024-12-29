5f808731fa5e3f823cd2b2817b207ccc8546c15a
Joo-Veloper <psjoo3515@gmail.com>
2024-12-29 14:31:38 +0900

    Easy Algorithm Learning - 02

---
+++ src/EAL/P02_Upper.java
@@ -0,0 +1,24 @@
+package EAL;
+
+import java.util.Scanner;
+
+public class P02_Upper {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word = sc.next();
+        System.out.println(soultion(word));
+    }
+
+    private static String soultion(String word) {
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (Character.isUpperCase(c)) {
+                answer.append(Character.toLowerCase(c));
+            } else if (Character.isLowerCase(c)) {
+                answer.append(Character.toUpperCase(c));
+            }
+        }
+        return answer.toString();
+    }
+}
