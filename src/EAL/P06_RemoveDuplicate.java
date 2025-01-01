43c7409427356d1e7dcee1e601231833a409e9df
Joo-Veloper <psjoo3515@gmail.com>
2025-01-01 19:28:26 +0900

    Easy Algorithm Learning - 06

---
+++ src/EAL/P06_RemoveDuplicate.java
@@ -0,0 +1,27 @@
+package EAL;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class P06_RemoveDuplicate {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(solution(str));
+    }
+
+    private static StringBuilder solution(String str) {
+        StringBuilder sb = new StringBuilder();
+        char[] array = str.toCharArray();
+        ArrayList<Character> list = new ArrayList<>();
+        for (int i = 0; i < array.length; i++) {
+            if (!list.contains(array[i])) {
+                list.add(array[i]);
+            }
+        }
+        for (int i = 0; i < list.size(); i++) {
+            sb.append(list.get(i));
+        }
+        return sb;
+    }
+}
