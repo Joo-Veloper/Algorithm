0b6194fb344f00b1b4c7cdbd9fe0efe3c918fc04
Joo-Veloper <psjoo3515@gmail.com>
2025-01-02 19:52:46 +0900

    Easy Algorithm Learning - 07(PalindromicString)

---
+++ src/EAL/P07_PalindromicString.java
@@ -0,0 +1,23 @@
+package EAL;
+
+import java.util.Scanner;
+
+public class P07_PalindromicString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(soultion(str));
+    }
+
+    private static String soultion(String str) {
+        str = str.toLowerCase();
+        int len = str.length();
+
+        for (int i = 0; i < len / 2; i++) {
+            if (str.charAt(i) != str.charAt(len - i - 1)) {
+                return "NO";
+            }
+        }
+        return "YES";
+    }
+}
