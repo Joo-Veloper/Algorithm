aa993f60c561c4e29e2023f35eeea30ddaea6de3
Joo-Veloper <psjoo3515@gmail.com>
2025-01-02 19:53:03 +0900

    Easy Algorithm Learning - 07(ValidPalindrome)

---
+++ src/EAL/P08_ValidPalindrome.java
@@ -0,0 +1,23 @@
+package EAL;
+
+import java.util.Scanner;
+
+public class P08_ValidPalindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(solution(str));
+    }
+
+    private static String solution(String str) {
+        str = str.toUpperCase().replaceAll("[^A-Z]", "");
+        int len = str.length();
+
+        for (int i = 0; i < len/2; i++) {
+            if (str.charAt(i) != str.charAt(len - i - 1)) {
+                return "NO";
+            }
+        }
+        return "YES";
+    }
+}
