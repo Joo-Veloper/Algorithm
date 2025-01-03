dcfc19832d9e2d77159d49d99f823a44de7cbdb2
Joo-Veloper <psjoo3515@gmail.com>
2025-01-03 19:11:32 +0900

    Easy Algorithm Learning - 08

---
+++ src/EAL/P09_ExtractOnlyNumbers.java
@@ -0,0 +1,22 @@
+package EAL;
+
+import java.util.Scanner;
+
+public class P09_ExtractOnlyNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(solution(str));
+    }
+
+    private static int solution(String str) {
+        String answer = "";
+        char[] charArray = str.toCharArray();
+        for (int i = 0; i < charArray.length; i++) {
+            if (Character.isDigit(charArray[i])) {
+                answer += charArray[i];
+            }
+        }
+        return Integer.parseInt(answer);
+    }
+}
