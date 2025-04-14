003703354fc40656567f097e49f5867d3062a61b
Joo-Veloper <psjoo3515@gmail.com>
2025-04-14 23:15:25 +0900

    Easy Algorithm Learning - Array P09 - Update

--- src/EAL/_02_array/P09_MaximumSumOfGridPlates.java
+++ src/EAL/_02_array/P09_MaximumSumOfGridPlates.java
@@ -7,59 +7,42 @@ public class P09_MaximumSumOfGridPlates {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        int[][] arr = new int[n][n]; // n * n 배열 만듬
+        int[][] arr = new int[n][n];
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 arr[i][j] = sc.nextInt();
             }
         }
+
         System.out.println(Solution(n, arr));
     }
 
     private static int Solution(int n, int[][] arr) {
-        int answer = 0;
-        // 행의 합
+        // 최댓값을 저장할 변수 아주 작은 수로 초기화
+        int answer = -2147000000;
+
+        // 각 행 열 합 저장
+        int sum1, sum2 = 0;
+
         for (int i = 0; i < n; i++) {
-            int row = 0;
+            // 0으로 초기화
+            sum1 = sum2 = 0;
             for (int j = 0; j < n; j++) {
-                row += arr[i][j];
+                sum1 += arr[i][j];
+                sum2 += arr[j][i];
             }
-            if (row > answer) {
-                answer = row;
-            }
+            answer = Math.max(answer, sum1);
+            answer = Math.max(answer, sum2);
         }
-
-        // 열의 합
-        for (int j = 0; j < n; j++) {
-            int column = 0;
-            for (int i = 0; i < n; i++) {
-                column += arr[i][j];
-            }
-            if (column > answer) {
-                answer = column;
-            }
-        }
-
-        // 대각선의 합1
-        int cross1 = 0;
+        // 0으로 초기화
+        sum1 = sum2 = 0;
         for (int i = 0; i < n; i++) {
-            cross1 += arr[i][i];
+            sum1 += arr[i][i];
+            sum2 += arr[i][n - i - 1];
         }
-        if (cross1 > answer) {
-            answer = cross1;
-        }
-
-        // 대각선의 합2
-        int cross2 = 0;
-        for (int i = 0; i < n; i++) {
-            cross2 += arr[i][n - 1 - i];
-        }
-        if (cross2 > answer) {
-            answer = cross2;
-        }
-
+        answer = Math.max(answer, sum1);
+        answer = Math.max(answer, sum2);
         return answer;
     }
-
 }
