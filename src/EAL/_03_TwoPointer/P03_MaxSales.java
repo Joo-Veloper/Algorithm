9e651b7e96decfb5d8ba019aafe33b64afb8ea7b
Joo-Veloper <psjoo3515@gmail.com>
2025-04-22 14:56:51 +0900

    Easy Algorithm Learning - TP P03 - update

--- src/EAL/_03_TwoPointer/P03_MaxSales.java
+++ src/EAL/_03_TwoPointer/P03_MaxSales.java
@@ -5,18 +5,15 @@ import java.util.Scanner;
 public class P03_MaxSales {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        // N일
+        // N일 동안 매출기록
         int n = sc.nextInt();
-        // K일
+        // 연속된 K일 동안
         int k = sc.nextInt();
 
-        // N일 매출 기록
         int[] a = new int[n];
-
         for (int i = 0; i < n; i++) {
             a[i] = sc.nextInt();
         }
-
         System.out.println(solution(n,k,a));
     }
 
@@ -31,8 +28,14 @@ public class P03_MaxSales {
         answer += sum;
 
         for (int i = k; i < n; i++) {
-            sum+=(a[i]-a[i-k]);
-            answer=Math.max(answer, sum);
+            // 배열: a = [3, 5, 2, 6, 1], k = 3
+            //처음 구간: 3 + 5 + 2 = 10
+            //다음: 10 - 3 + 6 = 13
+            //다음: 13 - 5 + 1 = 9
+            // 현재합 - 빠진합 + 새로들어온합
+            sum = sum - a[i - k] + a[i];
+
+            answer = Math.max(answer, sum);
         }
         return answer;
     }
