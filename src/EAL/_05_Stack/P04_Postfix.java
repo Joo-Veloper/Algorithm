4204d5bb5824f89c7dd1dd4c5200a2407d49d7a2
Joo-Veloper <psjoo3515@gmail.com>
2025-05-23 20:33:39 +0900

    Easy Algorithm Learning - Stack P04

---
+++ src/EAL/_05_Stack/P04_Postfix.java
@@ -0,0 +1,37 @@
+package EAL._05_Stack;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class P04_Postfix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        System.out.println(solution(str));
+    }
+
+    private static int solution(String str) {
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+        for (char c : str.toCharArray()) {
+            if (Character.isDigit(c)) {
+                stack.push(c - '0');
+            } else {
+                int rt = stack.pop();
+                int lf = stack.pop();
+                if(c=='+') {
+                    stack.push(lf + rt);
+                } else if(c=='-') {
+                    stack.push(lf - rt);
+                } else if(c=='*') {
+                    stack.push(lf * rt);
+                } else if(c=='/') {
+                    stack.push(lf / rt);
+                }
+            }
+        }
+        answer=stack.get(0);
+        return answer;
+    }
+}
