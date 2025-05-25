e4bc7ccb5f038810b0a90bc1695d3235ef8355da
Joo-Veloper <psjoo3515@gmail.com>
2025-05-25 15:46:44 +0900

    Inner(Lv1)

---
+++ src/pg/Lv1/Inner.java
@@ -0,0 +1,31 @@
+package pg.Lv1;
+
+import java.util.Scanner;
+
+public class Inner {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        int[] b = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            b[i] = sc.nextInt();
+        }
+
+        System.out.println(solution(a, b));
+    }
+
+    private static int solution(int[] a, int[] b) {
+        int answer = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            answer += a[i] * b[i];
+        }
+
+        return answer;
+    }
+}
